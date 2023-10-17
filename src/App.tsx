e04da10f510ffd6a839cd6f8763e1246638f7395
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-10-17 17:51:59 +0300

    Organization of styles. Adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { Suspense } from "react";
-import { Counter } from "./Counter";
-import "./../index.scss";
+import { Suspense, useContext, useState } from "react";
+import { Counter } from "./components/Counter";
 import { Route, Routes, Link } from "react-router-dom";
-import { AboutPageAsync } from "./Page/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./Page/MainPage/MainPage.async";
+import { AboutPageAsync } from "./Pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./Pages/MainPage/MainPage.async";
+import "./styles/index.scss";
+import { ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 function App() {
+  const { theme, handleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={handleTheme}>Сменить тему</button>
       <Link className="link" to={"/counter"}>
         Counter
       </Link>
       <Link className="link" to={"/about"}>
-        About
+        ABOUT
       </Link>
       <Link className="link" to={"/"}>
         Main
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,16 @@
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import { useContext } from "react";
+interface useThemeResult {
+  handleTheme: () => void;
+  theme: Theme;
+}
+export function useTheme(): useThemeResult {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const handleTheme = () => {
+    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    setTheme(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  };
+  return { theme, handleTheme };
+}
