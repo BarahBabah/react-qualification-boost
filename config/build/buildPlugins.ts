73343d826e7e3e9cc6b6bd2daf5574aae8bc9794
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-10-31 04:51:48 +0300

    i18n in webpack config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import { BuildOptions } from "./types/config";
 import miniCssExtractPlugin from "mini-css-extract-plugin";
 export function buildPlugins({
   paths,
+  isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HTMLWebpackPlugin({
@@ -14,6 +15,9 @@ export function buildPlugins({
     new miniCssExtractPlugin({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
 }
